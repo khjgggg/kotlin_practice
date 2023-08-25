47157ff49bb23829a6d6d42261393034a54e3017
khjgggg <guswjd7@gmail.com>
2023-08-25 11:50:58 +0900

    [FIX] : 🐛(master): 수정 1. SDK33이상 에뮬에서 실시간 알림 권한 받기 2. 알림 누르면 헤드업으로 뜨게하기

--- NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.kt
+++ NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.kt
@@ -4,21 +4,28 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.media.AudioAttributes
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.example.notificationtest.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        checkPermission()
+
         binding.notificationButton.setOnClickListener{
             notification()
         }
@@ -35,7 +42,7 @@ class MainActivity : AppCompatActivity() {
             val channel = NotificationChannel(
                 channelId,
                 channelName,
-                NotificationManager.IMPORTANCE_DEFAULT
+                NotificationManager.IMPORTANCE_HIGH
             ).apply {
                 // 채널에 다양한 정보 설정
                 description = "My Channel One Description"
@@ -70,6 +77,7 @@ class MainActivity : AppCompatActivity() {
             setContentTitle("새로운 알림입니다.")
             setContentText("알림이 잘 보이시나요.")
             setContentIntent(pendingIntent)
+            priority = NotificationCompat.PRIORITY_HIGH
             setStyle(NotificationCompat.BigTextStyle()
                 .bigText("이것은 긴텍스트 샘플입니다. 아주 긴 텍스트를 쓸때는 여기다 하면 됩니다.이것은 긴텍스트 샘플입니다. 아주 긴 텍스트를 쓸때는 여기다 하면 됩니다.이것은 긴텍스트 샘플입니다. 아주 긴 텍스트를 쓸때는 여기다 하면 됩니다."))
             setLargeIcon(bitmap)
@@ -82,5 +90,56 @@ class MainActivity : AppCompatActivity() {
 
         manager.notify(11, builder.build())
     }
+
+    private val ACTIVITY_REQUEST_PERMISSION = 1
+
+    private fun checkPermission() {
+        val permissions = getPermissions()
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(permissions, ACTIVITY_REQUEST_PERMISSION)
+                return
+            }
+        }
+    }
+
+    private fun getPermissions(): Array<String> {
+        val permissions = ArrayList<String>()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permissions.add(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
+
+        val arrPermission = arrayOfNulls<String>(permissions.size)
+        return permissions.toArray(arrPermission)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (grantResults.isEmpty()) {
+            Toast.makeText(this, "권한 결과 데이타가 없음", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (requestCode == ACTIVITY_REQUEST_PERMISSION) {
+            var isGrantedNoti = false
+            for (index in grantResults.indices) {
+                if (permissions[index] == android.Manifest.permission.POST_NOTIFICATIONS
+                    && grantResults[index] == PackageManager.PERMISSION_GRANTED) {
+                    isGrantedNoti = true
+                }
+            }
+
+            if(!isGrantedNoti) {
+                Toast.makeText(this, "알림 권한이 필요합니다. 설정에서 알림 권한을 켜주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 
 }
